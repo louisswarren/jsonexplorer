8820a3283b5ac2f3414fbc127c8d5ad0704f01e1
Louis Warren <louis@lsw.nz>
2023-12-26 22:17:52 +1300

    Add selector

--- jsonexplorer.py
+++ jsonexplorer.py
@@ -2,7 +2,7 @@ import sys
 import json
 import curses
 
-def jsonmenu(stdscr, j):
+def jsonmenu(stdscr, j, selector=''):
     curses.curs_set(False)
     sel = 0
     pressed = None
@@ -10,10 +10,14 @@ def jsonmenu(stdscr, j):
     while True:
         stdscr.clear()
 
-        menu_width = min(30, curses.COLS / 2)
+        menu_width = min(30, curses.COLS // 2)
 
-        for i in range(curses.LINES):
+        stdscr.addstr(0, 0,
+            selector[max(0, len(selector)-curses.COLS):] if selector else '.')
+        for i in range(2, curses.LINES):
             stdscr.addch(i, menu_width - 1, '|')
+        for i in range(curses.COLS):
+            stdscr.addch(1, i, '-')
         stdscr.refresh()
 
         elem = False
@@ -31,13 +35,13 @@ def jsonmenu(stdscr, j):
             menu.addstr(i, 0, item[0])
 
         menu.addstr(sel, 0, items[sel][0], curses.A_REVERSE)
-        menu.refresh(0, 0, 0, 0, curses.LINES - 1, menu_width - 2)
+        menu.refresh(0, 0, 2, 0, curses.LINES - 1, menu_width - 2)
 
         outlines = json.dumps(items[sel][1], indent=4).split('\n')
         disp = curses.newpad(len(outlines), max(len(x) for x in outlines)+1)
         for i, line in enumerate(outlines):
             disp.addstr(i, 0, line)
-        disp.refresh(0, 0, 0, menu_width + 1, curses.LINES - 1, curses.COLS - 1)
+        disp.refresh(0, 0, 2, menu_width + 1, curses.LINES - 1, curses.COLS - 1)
 
         while True:
             dims = curses.LINES, curses.COLS
@@ -56,7 +60,8 @@ def jsonmenu(stdscr, j):
                 case 'l':
                     pressed = None
                     if not elem:
-                        while not jsonmenu(stdscr, items[sel][1]):
+                        s = '.' + items[sel][0]
+                        while not jsonmenu(stdscr, items[sel][1], selector + s):
                             pass
                         break
                 case 'h':
@@ -69,19 +74,19 @@ def jsonmenu(stdscr, j):
             pressed = None
 
             menu.addstr(sel, 0, items[sel][0], curses.A_REVERSE)
-            if sel < curses.LINES - 4 or len(items) < curses.LINES:
+            if sel < curses.LINES - 6 or len(items) < curses.LINES:
                 scroll = 0
             else:
-                scroll = min(sel + 4 - curses.LINES, len(items) - curses.LINES)
-            menu.refresh(scroll, 0, 0, 0, curses.LINES - 1, menu_width - 2)
+                scroll = min(sel + 6 - curses.LINES, len(items) - curses.LINES + 2)
+            menu.refresh(scroll, 0, 2, 0, curses.LINES - 1, menu_width - 2)
 
             outlines = json.dumps(items[sel][1], indent=4).split('\n')
             disp.clear()
-            disp.refresh(0, 0, 0, menu_width, curses.LINES - 1, curses.COLS - 1)
+            disp.refresh(0, 0, 2, menu_width, curses.LINES - 1, curses.COLS - 1)
             disp = curses.newpad(len(outlines), max(len(x) for x in outlines)+1)
             for i, line in enumerate(outlines):
                 disp.addstr(i, 0, line)
-            disp.refresh(0, 0, 0, menu_width + 1, curses.LINES - 1, curses.COLS - 1)
+            disp.refresh(0, 0, 2, menu_width + 1, curses.LINES - 1, curses.COLS - 1)
 
 
 if __name__ == '__main__':
